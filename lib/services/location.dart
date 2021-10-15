d92fdcb8bff665f2a7b33b857a3c9efb3bf7ca37
Sahib Nanda <70871918+sahibnanda@users.noreply.github.com>
2021-10-15 17:27:30 -0400

    Add files via upload

---
+++ lib/services/location.dart
@@ -0,0 +1,15 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double longitude;
+  double latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      longitude = position.longitude;
+      latitude = position.latitude;
+    } catch (e) {}
+  }
+}
